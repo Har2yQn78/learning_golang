da06701a8e893d973431413b699a56aa427b3151
Har2yQn78 <hamidreza.amiri800@gmail.com>
2025-09-07 22:51:00 +0330

    for loop

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ var globalname string = "Global" // this is a gloabl var and can be used in all 
 
 func main() {
 	var name string = "Harry"
-	 var greeting = "Hello World"
+	var greeting = "Hello World"
 	// var name3 = "Jass" // can be use without saying it is string because we make a string var once inside the func
 	var number int = 23 // can use int8-16-32-64 for memory
 	//var number2 uint = 22 // can not use as negetive number
 	var number3 float64 = 74691.3757 // can only have float64 and float32
-	name2 := "AI" // this can only be used inside the function
+	name2 := "AI"                    // this can only be used inside the function
 	// Arrays
 	var numbers [6]int = [6]int{23, 25, 27, 28, 32, 56}
 	numbers[5] = 52
@@ -23,6 +23,7 @@ func main() {
 	scores = append(scores, 14)
 	rangenumbers := numbers[0:3]
 	ages := []int{25, 23, 24, 22, 21, 45, 56, 75, 78}
+	i := 0
 
 	sort.Ints(ages) // sort numbers in order  -  can also worked for string  - typical basic methods
 	index := sort.SearchInts(ages, 24)
@@ -37,9 +38,59 @@ func main() {
 	fmt.Println(strings.Contains(greeting, "Hello")) // just some basic import and package
 	fmt.Println(strings.ReplaceAll(greeting, "World", "Harry"))
 	fmt.Println(strings.ToUpper(greeting)) //typical upper method in all lang
-	fmt.Println(strings.Index(greeting, "W")) 
+	fmt.Println(strings.Index(greeting, "W"))
 	fmt.Println(ages)
 	fmt.Println(index)
+
+	for i <= 3 {
+		fmt.Println("i =", i)
+		i++
+	}
+
+	for f := 0; f<4; f++{
+		fmt.Println("f =", f)
+		f++
+	}
+
+	for  i := 1; i <= 3; i++ {
+        // Inner loop: controls columns
+        for j := 1; j <= 3; j++ {
+            fmt.Printf("%d × %d = %d\t", i, j, i*j)
+        }
+        fmt.Println() 
+    }
+
+	size := 4 
+
+    for i := 0; i < size; i++ {         
+        for j := 0; j < size; j++ {     
+            fmt.Print("* ")             
+        }
+        fmt.Println()                  
+    }
+	
+
+	for i := 1; i <= 3; i++ {
+        for j := 3; j > i; j-- {
+            fmt.Print(" ")
+        }
+        for k := 1; k <= i*2-1; k++ {
+            fmt.Print("*")
+        }
+        fmt.Println()
+    }
+
+    for i := 2; i >= 1; i-- {
+        for j := 3; j > i; j-- {
+            fmt.Print(" ")
+        }
+        for k := 1; k <= i*2-1; k++ {
+            fmt.Print("*")
+        }
+        fmt.Println()
+    }
+
+
 
 	// fmt.Print("Hello") // without ln, dosent add a new line
 	// fmt.Print(name)
