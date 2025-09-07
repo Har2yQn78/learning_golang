7801757f33071f3d290c02f4ad0864d95fc8509c
Har2yQn78 <hamidreza.amiri800@gmail.com>
2025-09-07 13:44:22 +0330

    format string

--- main.go
+++ main.go
@@ -7,16 +7,23 @@ var globalname string = "Global" // this is a gloabl var and can be used in all 
 func main() {
 	var name string = "Harry"
 
-	var name3 = "Jass" // can be use without saying it is string because we make a string var once inside the func
+	// var name3 = "Jass" // can be use without saying it is string because we make a string var once inside the func
 
 	var number int = 23 // can use int8-16-32-64 for memory
 
-	var number2 uint = 22 // can not use as negetive number
+	//var number2 uint = 22 // can not use as negetive number
 
-	var number3 float64 = 22.23 // can only have float64 and float32
+	var number3 float64 = 74691.3757 // can only have float64 and float32
 
 	name2 := "AI" // this can only be used inside the function
 
-	fmt.Println("Hello", name, name2, name2, number, globalname, name3, number2, number3)
+	fmt.Printf("Hello my name is %v, my age is somrthing betwwen %v and %v and you know what %v %v \n",
+	name, number, number3, name2, name2)
+	fmt.Print("Lets Rock")
+	fmt.Println("\nOK?")
+
+	// fmt.Print("Hello") // without ln, dosent add a new line
+	// fmt.Print(name)
+
 }
 
