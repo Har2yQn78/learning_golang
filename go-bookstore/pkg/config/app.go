328bb1bf4437069d8f2c8fc6ff268e748d88b363
Har2yQn78 <hamidreza.amiri800@gmail.com>
2025-09-23 15:12:20 +0330

    fix connection to sql

--- go-bookstore/pkg/config/app.go
+++ go-bookstore/pkg/config/app.go
@@ -11,7 +11,7 @@ var (
 
 func Connect() {
 	// DSN format: user:password@tcp(host:port)/dbname?params
-	dsn := "Harry:Harry1234@tcp(127.0.0.1:3306)/simplerest?charset=utf8mb4&parseTime=True&loc=Local" // in new update this is the new way to open
+	dsn := "Harry:Harry1234@tcp(127.0.0.1:3306)/simplerest?charset=utf8mb4&parseTime=True&loc=Local"// in new update this is the new way to open
 
 	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
