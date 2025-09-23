6b306868340ca023c832a0def2fccc6a64b697f8
Har2yQn78 <hamidreza.amiri800@gmail.com>
2025-09-23 14:57:10 +0330

    make a simple book managmeny system with connection to sql

--- go-bookstore/cmd/main/main.go
+++ go-bookstore/cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import(
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/Har2yQn78/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegiesterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
